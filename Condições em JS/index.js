8e254aae1d694854c6fb4bfc4edc2b9dcd25f702
Henrique Silva <henriiisilva2007@gmail.com>
2025-02-08 23:07:17 -0300

    Algumas praticas de condições em js

---
+++ Condições em JS/index.js
@@ -0,0 +1,9 @@
+//rodando este codigo em NODE
+
+let vel = 70.5
+console.log(`A velocidade do seu carro é ${vel}KM/H`)
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+
+}
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ Condições em JS/index02.js
@@ -0,0 +1,8 @@
+//rodando em NODE
+let país = 'França'
+console.log(`Vivendo em ${país}`)
+if (país != 'Brasil') {
+    console.log('Voce é Estrangeiro')
+}else {
+    console.log('Voce é Brasileiro')
+}
